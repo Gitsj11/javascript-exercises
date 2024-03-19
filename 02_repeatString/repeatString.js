6d2a941cefc139404469581380d40284b9b0c6c3
Gitsj11 <jsapini0719@gmail.com>
2024-03-19 12:13:09 -0400

    Completed exercises 1 and 2.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,19 @@
-const repeatString = function() {
+const repeatString = function(str, num) {
+    if (num < 0) {
+        return "ERROR";
+    } else if (num === 0) {
+        return str = "";
+    }
 
+    for (i = 1; i < num; i++) {
+        if (str === "") {
+            return str;
+        }
+        else {
+            str += "hey";
+        } 
+    }
+    return str;
 };
 
 // Do not edit below this line
