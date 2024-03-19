25cdcf53484880014d695507f85de1f8cc9f306e
Gitsj11 <jsapini0719@gmail.com>
2024-03-19 12:42:47 -0400

    Completed javascript exercise 3.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str === "") return str;
+    let arr = Array.from(str);
+    let revString = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        revString += arr[i];
+    }
+    return revString;
 };
 
 // Do not edit below this line
