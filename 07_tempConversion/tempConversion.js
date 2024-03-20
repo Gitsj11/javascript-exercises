4c8431408d2698689bff7bd5880855c8adcafefd
Gitsj11 <jsapini0719@gmail.com>
2024-03-20 15:58:45 -0400

    Completed exercise 7. Used Math.round method to prevent loss of precision with negative numbers.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(x) {
+    let y = (x - 32) * 5/9;
+    let result = Math.round(y * 10) / 10;
+    return result;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(y) {
+    let x = (9/5 * y) + 32;
+    let result = Math.round(x * 10) / 10;
+    return result;
 };
 
 // Do not edit below this line
