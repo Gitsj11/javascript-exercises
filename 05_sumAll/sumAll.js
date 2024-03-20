689216dc0391aa9a94f75f7981abe6ae13ff2cd5
Gitsj11 <jsapini0719@gmail.com>
2024-03-20 11:58:38 -0400

    Completed exercise 5. Added two funnctions to handle negative numbers and non-integer numbers. Calculated finalSum using Arithmetic Operators.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,10 +1,18 @@
-const sumAll = function(startNum, stopNum) {
+const sumAll = function(num1, num2) {
     let finalSum = 0;
-    while (startNum < stopNum) {
-        let sum = startNum + stopNum;
-        startNum++;
-        stopNum--;
-        finalSum += sum;
+    function isPositive() {
+        if (num1 >= 0 && num2 >= 0) return true;
+    }
+    function isNumber() {
+        if (Number.isInteger(num1) && Number.isInteger(num2)) return true;
+    }
+    if (!isPositive() || !isNumber()) return "ERROR";
+
+    if (num1 > num2) {
+        finalSum = ((num1 + num2) * num1) / 2;
+    }
+    else {
+        finalSum = ((num1 + num2) * num2) / 2;
     }
     return finalSum;
 };
