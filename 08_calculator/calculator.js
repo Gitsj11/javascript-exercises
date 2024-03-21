a2f61f738e5ac494d71cba39290ca6a2fc3c22d6
Gitsj11 <jsapini0719@gmail.com>
2024-03-20 20:45:04 -0400

    Completed exercise 8.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(x, y) {
+	  return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+    return x - y;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+    let result = 0;
+    if (arr.length === 0) {
+      return result;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        result += arr[i];
+    }
+    return result;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+    let result = 1;
+    for (let i = 0; i < arr.length; i++) {
+      result *= arr[i];
+    }
+    return result;
 };
 
-const power = function() {
-	
+const power = function(num, pow) {
+    return num ** pow;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+    //let sum = 1;
+    if (num < 0) return "Invalid Number";
+    else if (num === 0) return 1;
+    else return (num * factorial(num - 1));
 };
 
 // Do not edit below this line
