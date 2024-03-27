33e3b129ddce42837471bd874e47250e2c0a85a7
Gitsj11 <jsapini0719@gmail.com>
2024-03-27 12:27:30 -0400

    Completed exercise 10.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function(numOfTerms) {
+    let n = parseInt(numOfTerms);
+    if (n < 0) return "OOPS";
+    if (n === 0 || n === 1) return n;
+    let f0 = 0; //First Fibonacci Number
+    let f1 = 1; //Second Fibonacci Number
+    let nextNum = 0;
+    for (i = 1; i < n; i++) {
+        nextNum = f0 + f1;
+        f0 = f1;
+        f1 = nextNum;
+    }
+    return nextNum;
 };
 
 // Do not edit below this line
