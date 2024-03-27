7b46f542563e4508f3ca4a2fdb20eca7aa8c8f44
Gitsj11 <jsapini0719@gmail.com>
2024-03-27 11:21:44 -0400

    Completed exercise 9.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    var regex = /[^A-Za-z0-9]/g;
+    let lowRegStr = str.toLowerCase().replace(regex, ''); //Removes all non-alphanumeric characters from the string
+    let arr = lowRegStr.split(''); //Returns an array containing each element in the string
+    let newArr = [];
+    for (let i = arr.length - 1; i >= 0; i--) { //Loops from end of array to beginning
+        newArr.push(arr[i]); //Adds each element from old array to the end of new array
+    }
+    let revString = newArr.join(''); //Returns the new array as String
+    if (revString === lowRegStr) { //Compares new string and old string excluding alphanumeric characters
+        return true;
+    }
+    else return false; 
 };
 
 // Do not edit below this line
